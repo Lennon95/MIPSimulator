8c794b489ff07350f78a884b277c0d6cb84572ce
Lennon da Silva Rocha <lennon.silva@kinghost.com.br>
2019-05-22 20:27:47 -0300

    Continuando a fazer uns testes

--- tests/registerUnitTest.c
+++ tests/registerUnitTest.c
@@ -1,1 +1,30 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
 #include "../src/mips/datapath/register.h"
+
+void populateRegister(registerFile *r)
+{
+	for (size_t i = 0; i < TOTAL_GENERAL_REGISTERS; i++)
+		r->genPurpose[i] = 10 * (i + 1);
+}
+
+bool testSetGetReg()
+{
+	registerFile *regFile = newRegisterFile();
+	
+	for (size_t i = 0; i < TOTAL_GENERAL_REGISTERS; i++)
+		setReg(regFile, i, 10 * (i + 1));
+
+	
+	for (size_t i = 0; i < TOTAL_GENERAL_REGISTERS; i++) {
+		if (getReg(regFile, i) != (10 * (i + 1))) return false;
+	}
+	
+	return true;
+}
+
+void initTest()
+{
+}
