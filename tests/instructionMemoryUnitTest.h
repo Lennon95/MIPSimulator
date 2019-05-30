aabcea937c8d4e676798406545275440ef5f5381
Lennon da Silva Rocha <lennon.silva@kinghost.com.br>
2019-05-30 15:45:12 -0300

    Atualizando diretório de testes do datapath

--- tests/instructionMemoryUnitTest.h
+++
@@ -1,7 +0,0 @@
-#ifndef IMEMORY_UNIT_TEST
-#define IMEMORY_UNIT_TEST
-
-static bool testInitInstructionMem();
-extern bool instructionMemoryTests();
-
-#endif
--- tests/registerUnitTest.h
+++
@@ -1,8 +0,0 @@
-#ifndef REGISTER_UNIT_TEST
-#define REGISTER_UNIT_TEST
-
-static bool testCreateAndFreeReg();
-static bool testSetGetReg();
-extern bool registerTests();
-
-#endif
