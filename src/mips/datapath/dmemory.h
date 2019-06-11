4803cd62491b5a0597292730299dbe4394a4b151
Lennon da Silva Rocha <lennon.silva@kinghost.com.br>
2019-06-11 17:12:25 -0300

    Mudança na memória de dados

--- src/mips/datapath/dmemory.h
+++
@@ -1,9 +0,0 @@
-#ifndef DMEMORY_HEADER
-#define DMEMORY_HEADER
-
-// Declarations
-void initDataMem();
-int32_t readData(int32_t offset);
-void writeData(void* src, void* offset, size_t length);
-
-#endif
